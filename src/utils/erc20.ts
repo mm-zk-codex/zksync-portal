8400371aba634886d930ef9fb9772d28bcaae41b
mm <mm@matterlabs.dev>
2026-01-14 10:13:23 +0100

    small fixes

--- src/utils/erc20.ts
+++ src/utils/erc20.ts
@@ -1,5 +1,6 @@
 export const erc20Abi = [
   "function allowance(address owner, address spender) view returns (uint256)",
   "function approve(address spender, uint256 value) returns (bool)",
+  "function balanceOf(address owner) view returns (uint256)",
   "function decimals() view returns (uint8)"
 ];
